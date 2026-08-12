67cc0f0fb64e1a72e02b8456612a361279098b55
Suraj kumar <ksurajkumar336@gmail.com>
2026-08-12 10:06:23 +0530

    add new file

--- apps.js
+++ apps.js
@@ -1,6 +1,30 @@
 let url = "	https://api.adviceslip.com/advice";
 let btn = document.querySelector("#newAdviceBtn");
 let copybtn = document.querySelector("#copyBtn");
+let shareBtn = document.querySelector("#shareBtn");
+
+shareBtn.addEventListener("click",async()=>{
+  if (!document.querySelector("#adviceText")) {
+        alert("Please get an advice first!");
+        return;
+    }
+  try {
+    if(navigator.share) {
+      await navigator.share({
+        title: "AdviceHub",
+        text: `"${document.querySelector("#adviceText")}"`,
+      });
+    } else {
+      await navigator.clipboard.writeText(document.querySelector("#adviceText"));
+      alert("Sharing is not supported. Advice copied instead!");
+    }
+  } catch (error) {
+    console.log("Share cancelled or failed:", error);
+  }
+});
+
+// call Api
+
 btn.addEventListener("click",async()=> {
   let adviceFact = await GetAdvice();
 
@@ -8,18 +32,18 @@ btn.addEventListener("click",async()=> {
   document.querySelector("#adviceText").innerText = adviceFact.advice;
 
 
+// Copy text
+
  document.querySelector("small").textContent = ""; 
  document.querySelector("small").innerHTML = "Copy";
   copybtn.addEventListener("click",async()=> {
   try {
-    let advice64 = await GetAdvice();
+    // let advice64 = await GetAdvice();
     await navigator.clipboard.writeText(document.querySelector("#adviceText").textContent);
     document.querySelector("small").textContent = "Copied"; 
    
-
     setTimeout(()=> {
       document.querySelector("small").textContent = "copied";
-      // conso.log(copybtn);
     },2000);
 
   } catch (error) {
